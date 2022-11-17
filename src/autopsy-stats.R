e104759831a817756fab8ca7dd519e2a16811e6f
Corey Handelsman <chandelsman@gmail.com>
2022-11-17 08:22:59 -0700

    added code to write a list of stains to Excel

--- src/autopsy-stats.R
+++ src/autopsy-stats.R
@@ -3,32 +3,41 @@ library(here)
 library(tidyverse)
 
 # Import data
+# autopsies completed during billing cycle
 autopsies <- 
   readxl::read_excel(here("data", "2022.09.28-2022.10.27_autopsies.xls"))
+
+# Stains completed
 stains <- 
   list.files(
     path = here("data"),
     pattern = "^stain-data",
     full.names = TRUE
   ) %>% 
-  sapply(read_excel, 
+  sapply(readxl::read_excel, 
          simplify = FALSE,
   ) %>% 
   bind_rows()
-  # readxl::read_excel(here("data", "2022.09.12-2022.10.27_stains.xls"))
-  # readxl::read_excel(here("data", "stain-data-2022.10.27.xls"))
 
-# Clean stain data
+# Select stains performed for autopsies (Result ID starts with "AU")
 stain_count <- 
   stains %>% 
   filter(str_detect(`Result ID`, "^AU")) %>% 
   count(`Result ID`)
 
 # Join autopsy and stain data
-billing_data <- 
+block_stain_counts <- 
   autopsies %>% 
   left_join(stain_count, by = "Result ID") %>% 
   rename(`Stain Count` = n)
 
 # Output results to Excel
-writexl::write_xlsx(billing_data, here("output", "2022-10_autopsies.xlsx"))
+# Counts of blocks and stains performed on each autopsy
+writexl::write_xlsx(block_stain_counts, here("output", "2022-10_autopsy-counts.xlsx"))
+
+# Stains performed for each autopsy
+stains %>% 
+  filter(str_detect(`Result ID`, "^AU")) %>% select(`Result ID`, stain) %>% 
+  writexl::write_xlsx((here("output", "2022-10_autopsy-stains.xlsx")))
+  
+

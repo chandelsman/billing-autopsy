ac9d4f2494a18c124e48b92ac140dcf468d8c9a9
Corey Handelsman <chandelsman@gmail.com>
2023-12-12 08:26:43 -0700

    Github-Onedrive sync issues

--- src/autopsy-stats.R
+++ src/autopsy-stats.R
@@ -6,7 +6,7 @@ library(stringr)
 # Import data
 # autopsies completed during billing cycle
 autopsies <- 
-  readxl::read_excel(here("data", "2023.09.28-2023.10.27_autopsies.xls"))
+  readxl::read_excel(here("data", "2023.10.28-2023.11.27_autopsies.xls"))
 
 # Stains completed
 stains <- 
@@ -14,31 +14,31 @@ stains <-
     path = here("data"),
     pattern = "^stain-data",
     full.names = TRUE
-  ) %>% 
+  ) |> 
   sapply(readxl::read_excel, 
          simplify = FALSE,
-  ) %>% 
+  ) |> 
   bind_rows()
 
 # Select stains performed for autopsies (Result ID starts with "AU")
 stain_count <- 
-  stains %>% 
-  filter(str_detect(`Result ID`, "^AU")) %>% 
+  stains |> 
+  filter(str_detect(`Result ID`, "^AU")) |> 
   count(`Result ID`)
 
 # Join autopsy and stain data
 block_stain_counts <- 
-  autopsies %>% 
-  left_join(stain_count, by = "Result ID") %>% 
+  autopsies |> 
+  left_join(stain_count, by = "Result ID") |> 
   rename(`Stain Count` = n)
 
 # Output results to Excel
 # Counts of blocks and stains performed on each autopsy
-writexl::write_xlsx(block_stain_counts, here("output", "2023-10_autopsy-counts.xlsx"))
+writexl::write_xlsx(block_stain_counts, here("output", "2023-11_autopsy-counts.xlsx"))
 
 # Stains performed for each autopsy
-stains %>% 
-  filter(str_detect(`Result ID`, "^AU")) %>% select(`Result ID`, stain) %>% 
-  writexl::write_xlsx((here("output", "2023-10_autopsy-stains.xlsx")))
+stains |> 
+  filter(str_detect(`Result ID`, "^AU")) |> select(`Result ID`, stain) |> 
+  writexl::write_xlsx((here("output", "2023-11_autopsy-stains.xlsx")))
   
 
